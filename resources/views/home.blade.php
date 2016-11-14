b979dafe48380c62b13607df09df01783fe7e101
Fran <fchiari@hotmail.com>
2016-11-14 14:35:54 -0300

    Cambio del mapa por el de Leaflet + MapBox

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.maqueta')
 @section('content')
 <div class="container">
     <div class="row">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,33 @@
 @extends('layouts.app')
 @section('title', 'Home')
 @section('content')
+
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+  <script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
 <!-- Contenedor del Mapa  -->
 <div class="row">
-  <div id="map" class="col-xs-12"></div>
+  <div id="mapid" class="col-xs-12"></div>
 </div>
-<!-- Script del Mapa en JS -->
+
 <script>
+
+	var mymap = L.map('mapid').setView([-34.603722, -58.381592], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGV0bWFwIiwiYSI6ImNpdW4zdHowcDAwY2MydHBrNHdnenc3aXoifQ.m4atwfvT9jHj1Up4KKTjDw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+</script>
+
+
+
+
+<!-- Script del Mapa en JS -->
+{{-- <script>
   function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -30,7 +51,7 @@
 
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqIMwzgMBvFQl-PS1XtK16k30Ham_XDY8&callback=initMap"
-async defer></script>
+async defer></script> --}}
 
 
 
